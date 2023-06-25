365b148f6c10ca2dbd4aeed0a3822388c53347fd
wamboimargaret <margarettwamboi@gmail.com>
2023-06-25 20:44:45 +0300

    APIs

--- index.js
+++ index.js
@@ -1,59 +1,4 @@
-
-const tasksContainer = document.querySelector('.tasks');
-
-
-const form = document.querySelector('.add-task');
-form.addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
-
-    const input = this.querySelector('input[type="text"]');
-    const taskName = input.value.trim();
-
-    if (taskName !== '') {
-        addTask(taskName);
-        input.value = ''; 
-    }
-});
-
-function addTask(taskName) {
-   
-    const taskElement = document.createElement('div');
-    taskElement.classList.add('task');
-    
-   
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    
-   
-    const taskNameElement = document.createElement('span');
-    taskNameElement.textContent = taskName;
-
-  
-    const deleteButton = document.createElement('button');
-    deleteButton.classList.add('delete-button');
-    deleteButton.textContent = 'Delete';
-    
-    
-    deleteButton.addEventListener('click', function() {
-        taskElement.remove();
-    });
-    
-    
-    taskElement.appendChild(checkbox);
-    taskElement.appendChild(taskNameElement);
-    taskElement.appendChild(deleteButton);
-    
-    
-    tasksContainer.appendChild(taskElement);
-}
-
-
-
-
-
-
-
-const userContainer = document.getElementById('container');
+let userContainer = document.getElementById('container');
 const getUsers = () => {
   return fetch('https://dummyjson.com/todos?limit=22')
     .then(response => response.json())
@@ -63,18 +8,18 @@ const getUsers = () => {
 
 const displayUsers = async () => {
   const users = await getUsers();
-  console.log(users);
+  console.log(users.users);
   if (Array.isArray(users)) {
     users.forEach(item => {
       let div = document.createElement('div');
       let userName = document.createElement('input');
       let ids = document.createElement('span');
       let checkbox = document.createElement('input');
-      let icon = document.createElement('i');
+      let button = document.createElement('button');
       checkbox.type = 'checkbox';
       checkbox.checked = item.completed;
-      icon.classList.add('fa', 'fa-trash');
-      ids.appendChild(icon);
+      button.innerHTML = 'delete'
+      ids.appendChild(button);
       userName.value = item.todo;
       checkbox.addEventListener('change', () => {
         if (checkbox.checked) {
@@ -83,22 +28,22 @@ const displayUsers = async () => {
           userName.style.textDecoration = 'none';
         }
       });
-      icon.addEventListener('click', () => {
+      button.addEventListener('click', () => {
         deleteUser(item.id);
         div.remove();
       });
+
       div.appendChild(checkbox);
       div.appendChild(userName);
       div.appendChild(ids);
-      div.setAttribute('key', item.id);
-      div.setAttribute('class', 'people');
+     
       userContainer.appendChild(div);
     });
   }
 };
 const deleteUser = async (userId) => {
   try {
-    const response = await fetch(`https://dummyjson.com/todos/${userId}`, {
+    const response = await fetch(`https://dummyjson.com/todos/1`, {
       method: 'DELETE'
     });
     if (!response.ok) {
@@ -109,3 +54,41 @@ const deleteUser = async (userId) => {
   }
 };
 displayUsers();
+
+const addForm = document.getElementById('add-task');
+addForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const taskInput = document.getElementById('taskInput');
+  const newTask = taskInput.value;
+  taskInput.value = '';
+  if (newTask) {
+    const div = document.createElement('div');
+    const userName = document.createElement('input');
+    const ids = document.createElement('span');
+    const checkbox = document.createElement('input');
+    const button = document.createElement('button');
+    checkbox.type = 'checkbox';
+    checkbox.checked = false;
+    button.innerHTML = 'delete';
+     ids.appendChild(button);
+    userName.value = newTask;
+    checkbox.addEventListener('change', () => {
+      if (checkbox.checked) {
+        userName.style.textDecoration = 'line-through';
+      } else {
+        userName.style.textDecoration = 'none';
+      }
+    });
+
+    button.addEventListener('click', () => {
+      div.remove();
+    });
+
+    div.appendChild(checkbox);
+    div.appendChild(userName);
+    div.appendChild(ids);
+   
+    userContainer.prepend(div);
+  }
+})
+
